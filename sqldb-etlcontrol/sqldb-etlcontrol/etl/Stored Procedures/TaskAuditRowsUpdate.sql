57ea10ed20d5b825414460f7fe7758b4a801e241
joshuafennessy <jfennessy@blue-granite.com>
2020-04-20 00:27:09 -0400

    Updates to new proc for storing row counts

---
+++ sqldb-etlcontrol/sqldb-etlcontrol/etl/Stored Procedures/TaskAuditRowsUpdate.sql
@@ -0,0 +1,13 @@
+
+
+CREATE OR ALTER PROCEDURE [etl].[TaskAuditRowsUpdate] (
+	@TaskAuditKey INT, @RowsRead BIGINT, @RowsCopied BIGINT
+)
+AS
+
+UPDATE etl.TaskAudit
+SET 
+	RowsRead = @RowsRead,
+    RowsCopied = @RowsCopied,
+	LastUpdated = GETUTCDATE()	
+WHERE TaskAuditKey = @TaskAuditKey
